69bcc77455cbe1ea43c90ac3ca79904d692e335e
coryjpiette <60293516+coryjpiette@users.noreply.github.com>
2020-02-23 07:59:16 -0500

    Update run_Analysis.R

--- run_Analysis.R
+++ run_Analysis.R
@@ -1,37 +1,61 @@
-## Step 1
+# reading all the files into R
 
-X <- rbind(x_train, x_test)
-Y <- rbind(y_train, y_test)
-Subject <- rbind(subject_train, subject_test)
-Merged_Data <- cbind(Subject, Y, X)
+training_data <- read.table("C:/Users/shubhayush/Documents/coursera/data/getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/train/X_train.txt")
+test_data <- read.table("C:/Users/shubhayush/Documents/coursera/data/getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/test/X_test.txt")
+variable_names <- read.table("C:/Users/shubhayush/Documents/coursera/data/getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/features.txt")
+training_activity_codes <- read.table("C:/Users/shubhayush/Documents/coursera/data/getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/train/y_train.txt")
+test_activity_codes <- read.table("C:/Users/shubhayush/Documents/coursera/data/getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/test/y_test.txt")
+activity_names <- read.table("C:/Users/shubhayush/Documents/coursera/data/getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/activity_labels.txt")
+subject_train <- read.table("C:/Users/shubhayush/Documents/coursera/data/getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/train/subject_train.txt")
+subject_test <- read.table("C:/Users/shubhayush/Documents/coursera/data/getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/test/subject_test.txt")
 
-## Step 2
+# giving the column names to the training and test-data as per the variable_names
+variable_names_select <- variable_names[,2] 
+activity_labels <- activity_names[,2]
+colnames(training_data) <- variable_names_select
+colnames(test_data) <- variable_names_select
+colnames(training_activity_codes)<-"activitylabel"
+colnames(test_activity_codes)<-"activitylabel"
 
-TidyData <- Merged_Data %>% select(subject, code, contain("mean), contains("std"))
+# collecting and sorting the columns with mean and standard deviation
+ColstoSelect <- sort(c(grep("mean",variable_names[,2]),grep("std",variable_names[,2])))
 
-## Step 3
+# creating a column with activity names  
+training_activity_codes$activitylabel[training_activity_codes$activitylabel==1]<-"walking"
+training_activity_codes$activitylabel[training_activity_codes$activitylabel==2]<-"walkingUpstairs"
+training_activity_codes$activitylabel[training_activity_codes$activitylabel==3]<-"walkingDownstairs"
+training_activity_codes$activitylabel[training_activity_codes$activitylabel==4]<-"sitting"
+training_activity_codes$activitylabel[training_activity_codes$activitylabel==5]<-"standing"
+training_activity_codes$activitylabel[training_activity_codes$activitylabel==6]<-"laying"
 
-TidayData$code <- activities[TidyData$code, 2]
+test_activity_codes$activitylabel[test_activity_codes$activitylabel==1]<-"walking"
+test_activity_codes$activitylabel[test_activity_codes$activitylabel==2]<-"walkingUpstairs"
+test_activity_codes$activitylabel[test_activity_codes$activitylabel==3]<-"walkingDownstairs"
+test_activity_codes$activitylabel[test_activity_codes$activitylabel==4]<-"sitting"
+test_activity_codes$activitylabel[test_activity_codes$activitylabel==5]<-"standing"
+test_activity_codes$activitylabel[test_activity_codes$activitylabel==6]<-"laying"
 
-## Step 4
+# adding columns of the person/subject who took part in the activity and type of activity
 
-names(TidyData)[2] = "activity"
-names(TidyData)<-gsub("Acc", "Accelerometer", names(TidyData))
-names(TidyData)<-gsub("Gyro", "Gyroscope", names(TidyData))
-names(TidyData)<-gsub("BodyBody", "Body", names(TidyData))
-names(TidyData)<-gsub("Mag", "Magnitude", names(TidyData))
-names(TidyData)<-gsub("^t", "Time", names(TidyData))
-names(TidyData)<-gsub("^f", "Frequency", names(TidyData))
-names(TidyData)<-gsub("tBody", "TimeBody", names(TidyData))
-names(TidyData)<-gsub("-mean()", "Mean", names(TidyData), ignore.case = TRUE)
-names(TidyData)<-gsub("-std()", "STD", names(TidyData), ignore.case = TRUE)
-names(TidyData)<-gsub("-freq()", "Frequency", names(TidyData), ignore.case = TRUE)
-names(TidyData)<-gsub("angle", "Angle", names(TidyData))
-names(TidyData)<-gsub("gravity", "Gravity", names(TidyData))
+training_data <- cbind(subject_train,training_activity_codes$activitylabel,training_data)
+test_data <- cbind(subject_test,test_activity_codes$activitylabel,test_data)
 
-## Step 5
+# selecting columns pertaining to mean and standard devaiation to the dataset
+training_data_select <- training_data[,c(1,2,(ColstoSelect)+2)]
+test_data_select <- test_data[,c(1,2,(ColstoSelect)+2)]
+colnames(training_data_select)[1:2]<- c("subjectId","activity")
+colnames(test_data_select)[1:2]<- c("subjectId","activity")
 
-FinalData <- TidyData %>%
-    group_by(subject, activity) %>%
-    summarise_all(funs(mean))
-write.table(FinalData, "FinalData.txt", row.name=FALSE)
+# merging the train and  test data. Herein train and test  subjects can be identified   
+library(dplyr)
+training_data_select <- mutate(training_data_select,datatype = "train")
+test_data_select <- mutate(test_data_select,datatype = "test")
+final_data <- merge(training_data_select,test_data_select,all= TRUE)
+final_data <- arrange(final_data,subjectId)
+
+# finding mean as persubject and activity
+final_data1 <- select(final_data,-datatype)
+final_mean <- group_by(final_data1,subjectId,activity)
+final_means <- summarise_at(final_mean,vars(-group_cols()),mean)
+
+write.table(final_means,"Samsung_mean_data_MSP.txt",row.names = FALSE)
